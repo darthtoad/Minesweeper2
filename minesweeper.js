ed68c9e254114d3516a917c4d65bac9dc110ef63
darthtoad <30681859+darthtoad@users.noreply.github.com>
2017-08-24 11:43:31 -0500

    Add files via upload

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,5 @@
+/*
+First part of Codecademy project
 const blankLine = '  |   |  ';
 console.log("This is what an empty board would look like");
 console.log(blankLine);
@@ -8,4 +10,21 @@ const bombLine = '  | B |  ';
 console.log("This is what a board with a guess and a bomb on it would look like:");
 console.log(guessLine);
 console.log(bombLine);
-console.log(blankLine);
+console.log(blankLine); */
+const board = [ 
+	[' ', ' ', ' '],
+	[' ', ' ', ' '], 
+	[' ', ' ', ' ']
+];
+
+const printBoard = (board) => {
+	console.log("Current Board:");
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+
+printBoard(board);
+board[0][1] = '1';
+board[2][1] = 'B';
+printBoard(board);
